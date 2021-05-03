2bfe45a765eb4713f429620fc4f02d19663e315d
silvio usai <berzylius@hotmail.it>
2021-05-03 12:00:51 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -3,18 +3,30 @@ var app = new Vue(
         el: '#root',
         data: {
             movies: [],
+            userSearch: '',
         },
         methods: {
-
+            search() {
+                if(this.userSearch.length > 0) {
+                    axios
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: '7f4db2e84c6497f875534433bb139d8d',
+                        query: this.userSearch,
+                    }
+                })
+                
+                .then((response) => {
+                    const result = response.data.results;
+                    console.log(result);
+                    this.movies = result;
+                })
+                this.userSearch = '';
+                }
+                
+            }
         },
         mounted() {
-            axios
-            .get('https://api.themoviedb.org/3/search/movie?api_key=7f4db2e84c6497f875534433bb139d8d&query=star')
-            .then((response) => {
-                const result = response.data.results;
-                console.log(result);
-                this.movies = result;
-            });
         }
     }
 );
